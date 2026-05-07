bcfce96806095ecf65580f294b484cd81de9b6fc
usman-ghani564 <usmanghani564.ug9@gmail.com>
2026-05-08 00:24:22 +0500

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const logger = new AppLoggerService();
   const app = await NestFactory.create(AppModule, { logger });
 
+  app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter(logger));
   await app.listen(process.env.PORT ?? 3000);
 }
